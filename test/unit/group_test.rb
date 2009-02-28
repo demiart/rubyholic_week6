187c17a1b06a45cb65939cbe92946fdb9c47d8ee
Mathew Chasan <aftermathew@gmail.com>
2009-02-27 20:07:52 -0800

    added validation of url and some tests for it

--- test/unit/group_test.rb
+++ test/unit/group_test.rb
@@ -1,10 +1,51 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-  test  "name and only name must exist" do
-    bad_group = Group.new
-    bad_group.save
-    assert(bad_group.errors.on('name').to_a.include?("can't be blank"))
+  def test_validation_error klass, member, error_string
+    bad_object = klass.send :new
+    bad_object.save
+    assert(bad_object.errors.on(member).to_a.include?(error_string))
+  end
+
+  test "validates name" do
+    test_validation_error Group, 'name', "can't be blank"
+  end
+
+
+  def assert_url is_valid, group, url
+    group.website_url = url
+    group.save
+    errors = group.errors.on('website_url')
+    if(is_valid)
+      assert_nil(errors)
+    else
+      assert(errors)
+    end
+  end
+
+  test "website url can be blank" do
+    g = Group.new
+    assert_url true, g, ""
+  end
+
+  test "bad urls are bad" do
+    g = Group.new
+    assert_url false, g, 'a'
+    assert_url false, g, 'http:/bad.url.com'
+    assert_url false, g, 'http://bad'
+    assert_url false, g, 'http:/bad.url.com'
+    assert_url false, g, 'ftp://bad.url.com'
+    assert_url false, g, 'http://bad..com'
+  end
+
+  test "good urls are good"  do
+    g = Group.new
+    assert_url true, g, 'https://good.url.com'
+    assert_url true, g, 'http://yay.url.biz'
+    assert_url true, g, 'http://uwruby.com'
+    assert_url true, g, 'http://www.u123.com/'
+    assert_url true, g, 'http://bad.goawayforever.com/this/should/work.html'
+    assert_url true, g, 'https://happy.laughter.me'
   end
 
 end
