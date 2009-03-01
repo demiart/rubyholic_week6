e1339d6d73c0f5d862ac42f661d7e141836188e6
Demi <kirklandnokia@kirkland-nokias-macbook-pro.local>
2009-02-28 21:48:52 -0800

    added pretty date time printing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
 
   def get_nice_time(utctime)
-    ltime = utctime.localtime
+    ltime = utctime #.localtime
     wday = case ltime.wday
       when 1 then "Monday"
       when 2 then "Tuesday"
@@ -15,7 +15,7 @@ module ApplicationHelper
     ampm = ltime.hour >= 12 ? "PM" : "AM"
     hour = ltime.hour > 12 ? ltime.hour - 12 : ltime.hour
     hour = 12 if hour == 0
-    sprintf("%s, %d/%d/%d, %d:%02d:%02d %s", wday, ltime.mon, ltime.day, ltime.year, hour, ltime.min, ltime.sec, ampm)
+    sprintf("%s, %d/%d/%d, %d:%02d:%02d %s", wday, ltime.mon + 1, ltime.day, ltime.year, hour, ltime.min, ltime.sec, ampm)
   end
 
 end
