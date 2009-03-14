2af23dcb84afd560be11aa46a909f8abbe7f6c46
Demi <kirklandnokia@kirkland-nokias-macbook-pro.local>
2009-03-14 11:15:52 -0700

    using will_paginate, persisted location drop-down option in groups index over subsequent calls.

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,16 @@
 module GroupsHelper
+
+  def get_location_option_from_id(id)
+    if id then
+      begin
+        loc = Location.find(id)
+      rescue
+        loc = Location.first
+      end
+    else
+      loc = Location.first
+    end
+    [loc.name, loc.id]
+  end
+
 end
